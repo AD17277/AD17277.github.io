3f4831b15b2354634df93599386ff6c3bc370a58
AD17277 <daniele.ambietti@hotmail.it>
2024-12-23 21:56:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+// Cronologia dei menu visitati
+let menuHistory = [];
+
 // Variabile per il menu attuale
 let currentMenu = "main-menu";
 
 // Mostra un menu specifico
 function showMenu(menuId) {
+    // Aggiungi l'attuale menu alla cronologia prima di cambiare
+    if (currentMenu !== menuId) {
+        menuHistory.push(currentMenu);
+    }
+
+    // Nascondi il menu attuale e mostra quello selezionato
     document.getElementById(currentMenu).classList.add("hidden");
     document.getElementById(menuId).classList.remove("hidden");
     currentMenu = menuId;
@@ -10,13 +19,18 @@ function showMenu(menuId) {
 
 // Torna al menu precedente
 function goBack() {
-    if (currentMenu.endsWith("-menu")) {
-        const parentMenu = currentMenu.split("-")[0];
-        showMenu(parentMenu + "-menu");
+    if (menuHistory.length > 0) {
+        // Rimuovi l'ultimo elemento dalla cronologia e torna al menu precedente
+        const previousMenu = menuHistory.pop();
+        document.getElementById(currentMenu).classList.add("hidden");
+        document.getElementById(previousMenu).classList.remove("hidden");
+        currentMenu = previousMenu;
     }
 }
 
 // Torna alla homepage
 function goHome() {
+    // Resetta la cronologia e torna al menu principale
+    menuHistory = [];
     showMenu("main-menu");
 }
